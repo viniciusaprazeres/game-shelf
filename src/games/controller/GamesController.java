8460d94a78df604171d9b52286f1a12d92802b54
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-31 22:05:56 -0300

    Implementing the CRUD methods in the class GamesController

--- src/games/controller/GamesController.java
+++ src/games/controller/GamesController.java
@@ -1,4 +1,64 @@
 package games.controller;
 
-public class GamesController {
+import games.model.Games;
+import games.repository.GamesRepository;
+
+import java.util.ArrayList;
+
+public class GamesController implements GamesRepository {
+
+    private ArrayList<Games> gamesList = new ArrayList<Games>();
+
+    @Override
+    public void create(Games game) {
+        gamesList.add(game);
+        System.out.println("The game was registered!");
+    }
+
+    @Override
+    public void searchAll() {
+        for (var game: gamesList)
+            game.informations();
+    }
+
+    @Override
+    public void searchByName(String name) {
+        var game = searchInTheCollection(name);
+
+        if (game != null)
+            game.informations();
+        else
+            System.out.println("The game: " + name + " was not found!");
+    }
+
+    @Override
+    public void update(Games game) {
+        var searchGame = searchInTheCollection(game.getName());
+
+        if (searchGame != null){
+            gamesList.set(gamesList.indexOf(searchGame), game);
+            System.out.println("The game: " + game.getName() + " was updated!");
+        } else
+            System.out.println("The game: " + game.getName() + " was not found!");
+    }
+
+    @Override
+    public void delete(String name) {
+        var game = searchInTheCollection(name);
+
+        if (game != null){
+            if (gamesList.remove(game))
+                System.out.println("The game: " + name + " was deleted!");
+        } else
+            System.out.println("The game: " + name + " was not found!");
+    }
+
+    // Secondary methods
+    public Games searchInTheCollection(String name) {
+            if (gamesList.contains(name)) {
+                return gamesList.get(gamesList.indexOf(name));
+            }
+
+        return null;
+    }
 }
