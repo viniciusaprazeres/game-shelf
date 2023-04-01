7b12f923b5495f71a4f967bd3529959dfa6ef295
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-31 21:29:06 -0300

    Implementing the CRUD methods in the interface GamesRepository

--- src/games/repository/GamesRepository.java
+++ src/games/repository/GamesRepository.java
@@ -1,4 +1,13 @@
 package games.repository;
 
+import games.model.Games;
+
 public interface GamesRepository {
+
+    // CRUD methods
+    public void create(Games game);
+    public void searchAll();
+    public void searchByName(String name);
+    public void update(Games game);
+    public void delete(String name);
 }
